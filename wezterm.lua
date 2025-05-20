636f5e03f3590f5148a438188ef84cbdf7a61ca8
Marcin Makurat <mm@next-solution.pl>
2025-05-20 21:11:12 +0200

    Create wezterm.lua

---
+++ wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require("wezterm")
+
+return {
+  font = wezterm.font("JetBrains Mono"),
+  font_size = 14.0,
+  color_scheme = "Dracula",
+  enable_tab_bar = true,
+  window_decorations = "RESIZE",
+}
