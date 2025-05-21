81dab457b7da3ced13ab9cf6e06e3ce3e24886ca
Marcin Makurat <mm@next-solution.pl>
2025-05-21 21:50:24 +0200

    Create post-install.sh

---
+++ post-install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "🛠️ Downloading and installing Oh My Zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "🎨 Configuring theme and plugins..."
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' ~/.zshrc
+echo 'plugins=(git zsh-autosuggestions zsh-syntax-highlighting)' >> ~/.zshrc
+
+echo "✨ Updating shell..."
+source ~/.zshrc
+
+echo "Cloning Paru repository..."
+git clone https://aur.archlinux.org/paru.git
+
+echo "Entering Paru directory..."
+cd paru
+
+echo "Building and installing Paru..."
+makepkg -si --noconfirm
+cd..
+rm -rf paru
+echo "alias yay='paru'" >> ~/.zshrc
+source ~/.zshrc
+
+paru -S --noconfirm brave-bin
+
+echo "Installation completed!"
