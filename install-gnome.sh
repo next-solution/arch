bdf74fb2cfd135e7eed8950306e99ae5895ec25c
Marcin Makurat <mm@next-solution.pl>
2025-05-21 21:53:20 +0200

    Update install-gnome.sh

--- install-gnome.sh
+++ install-gnome.sh
@@ -12,10 +12,6 @@ sudo pacman -S --noconfirm gnome-control-center nautilus
 echo "Enabling GDM..."
 sudo systemctl enable gdm
 
-echo "Installing Dev tools..."
-sudo pacman -S --noconfirm wezterm
-sudo pacman -S --noconfirm --needed base-devel git
-
 echo "📦 Installing Zsh..."
 sudo pacman -S --noconfirm zsh
 
@@ -23,30 +19,5 @@ echo "⚙️ Setting Zsh as the default shell..."
 chsh -s $(which zsh)
 
 echo "🔄 Log out and log back in to activate Zsh."
-
-echo "🛠️ Downloading and installing Oh My Zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-echo "🎨 Configuring theme and plugins..."
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' ~/.zshrc
-echo 'plugins=(git zsh-autosuggestions zsh-syntax-highlighting)' >> ~/.zshrc
-
-echo "✨ Updating shell..."
-source ~/.zshrc
-
-echo "Cloning Paru repository..."
-git clone https://aur.archlinux.org/paru.git
-
-echo "Entering Paru directory..."
-cd paru
-
-echo "Building and installing Paru..."
-makepkg -si --noconfirm
-cd..
-rm -rf paru
-echo "alias yay='paru'" >> ~/.zshrc
-source ~/.zshrc
-
-paru -S --noconfirm brave-bin
 
 echo "Installation completed!"
