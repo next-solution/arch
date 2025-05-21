5682750317c20611368255bd47ef0858a7c91ff3
Marcin Makurat <mm@next-solution.pl>
2025-05-21 21:36:30 +0200

    Update install-gnome.sh

--- install-gnome.sh
+++ install-gnome.sh
@@ -16,6 +16,32 @@ echo "Installing Dev tools..."
 sudo pacman -S --noconfirm wezterm
 sudo pacman -S --noconfirm --needed base-devel git
 
+#!/bin/bash
+
+# Check if the script is running as root
+if [[ $EUID -ne 0 ]]; then
+   echo "Please run the script as root (sudo)." 
+   exit 1
+fi
+
+echo "📦 Installing Zsh..."
+pacman -S --noconfirm zsh
+
+echo "⚙️ Setting Zsh as the default shell..."
+chsh -s $(which zsh)
+
+echo "🔄 Log out and log back in to activate Zsh."
+
+echo "🛠️ Downloading and installing Oh My Zsh..."
+su -c 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
+
+echo "🎨 Configuring theme and plugins..."
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' ~/.zshrc
+echo 'plugins=(git zsh-autosuggestions zsh-syntax-highlighting)' >> ~/.zshrc
+
+echo "✨ Updating shell..."
+source ~/.zshrc
+
 echo "Cloning Paru repository..."
 git clone https://aur.archlinux.org/paru.git
 
@@ -26,9 +52,9 @@ echo "Building and installing Paru..."
 makepkg -si --noconfirm
 cd..
 rm -rf paru
-echo "alias yay='paru'" >> ~/.bashrc
-source ~/.bashrc
+echo "alias yay='paru'" >> ~/.zshrc
+source ~/.zshrc
 
 paru -S --noconfirm brave-bin
 
-echo "Installation completed. GNOME and NetworkManager have been successfully configured!"
+echo "Installation completed!"
