ea78124ee739721501139892492091d14c9707b7
Marcin Makurat <mm@next-solution.pl>
2025-05-22 09:09:34 +0200

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -2,10 +2,6 @@
 
 cd ~
 
-echo "Installing Dev tools..."
-sudo pacman -S --noconfirm wezterm
-sudo pacman -S --noconfirm --needed base-devel git
-
 echo "🛠️ Downloading and installing Oh My Zsh..."
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 
@@ -13,22 +9,9 @@ echo "🎨 Configuring theme and plugins..."
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' ~/.zshrc
 echo 'plugins=(git zsh-autosuggestions zsh-syntax-highlighting)' >> ~/.zshrc
 
+echo "alias yay='paru'" >> ~/.zshrc
+
 echo "✨ Updating shell..."
 source ~/.zshrc
-
-echo "Cloning Paru repository..."
-git clone https://aur.archlinux.org/paru.git
-
-echo "Entering Paru directory..."
-cd paru
-
-echo "Building and installing Paru..."
-makepkg -si --noconfirm
-cd..
-rm -rf paru
-echo "alias yay='paru'" >> ~/.zshrc
-source ~/.zshrc
-
-paru -S --noconfirm brave-bin
 
 echo "Installation completed!"
